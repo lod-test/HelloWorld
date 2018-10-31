3f399a9eb93c9eb025c6ff807fad8f4540813726
lod-test <b2b.lodhq@gmail.com>
2018-10-31 13:17:49 +0900

    Added code

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Welcome to LinuxOnDeX!");
+        System.out.println("Hello LinuxOnDex");
     }
 }
